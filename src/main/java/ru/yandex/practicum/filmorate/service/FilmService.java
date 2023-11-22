5ed77dec0cac5f6a23359087a27c926bf8a7c27a
Osdantes <121436438+Osdantes@users.noreply.github.com>
2023-11-22 19:53:57 +0300

    Update FilmService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.validate.Validate;
 
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
