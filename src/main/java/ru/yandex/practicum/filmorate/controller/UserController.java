c2d07a8f338bc7b84f124ba00e4722cb0728f36d
Osdantes <121436438+Osdantes@users.noreply.github.com>
2023-11-22 17:48:50 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -23,47 +23,47 @@ public class UserController {
 
     @Autowired
     public UserController(UserService userService) {
-            this.userService = userService;
-        }
-
-        @PostMapping
-        public User create (@Valid @RequestBody User user, BindingResult bindingResult){
-            return userService.create(user, bindingResult);
-        }
-
-        @PutMapping
-        public User update (@Valid @RequestBody User user, BindingResult bindingResult){
-            return userService.update(user, bindingResult);
-        }
-
-        @GetMapping
-        public List<User> getUsers () {
-            return userService.getUsers();
-        }
-
-        @GetMapping("/{id}")
-        public User getByIdUser ( @PathVariable long id){
-            return userService.getByIdUser(id);
-        }
-
-        @PutMapping("/{id}/friends/{friendId}")
-        public void addFriend ( @PathVariable("id") long idUser, @PathVariable("friendId") long idFriend){
-            userService.addFriend(idUser, idFriend);
-        }
-
-        @DeleteMapping("/{id}/friends/{friendId}")
-        public void deleteFriend ( @PathVariable("id") long idUser, @PathVariable("friendId") long idFriend){
-            userService.deleteFriend(idUser, idFriend);
-        }
-
-        @GetMapping("/{id}/friends")
-        public List<User> getUserFriends ( @PathVariable long id){
-            return userService.getUserFriends(id);
-        }
-
-        @GetMapping("/{id}/friends/common/{otherId}")
-        public List<User> getUserFriendsCommonWithOtherUser ( @PathVariable("id") long idUser,
-        @PathVariable("otherId") long idOtherUser){
-            return userService.getUserFriendsCommonWithOtherUser(idUser, idOtherUser);
-        }
+        this.userService = userService;
     }
+
+    @PostMapping
+    public User create(@Valid @RequestBody User user, BindingResult bindingResult) {
+        return userService.create(user, bindingResult);
+    }
+
+    @PutMapping
+    public User update(@Valid @RequestBody User user, BindingResult bindingResult) {
+        return userService.update(user, bindingResult);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getByIdUser(@PathVariable long id) {
+        return userService.getByIdUser(id);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable("id") long idUser, @PathVariable("friendId") long idFriend) {
+        userService.addFriend(idUser, idFriend);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable("id") long idUser, @PathVariable("friendId") long idFriend) {
+        userService.deleteFriend(idUser, idFriend);
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getUserFriends(@PathVariable long id) {
+        return userService.getUserFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getUserFriendsCommonWithOtherUser(@PathVariable("id") long idUser,
+                                                        @PathVariable("otherId") long idOtherUser) {
+        return userService.getUserFriendsCommonWithOtherUser(idUser, idOtherUser);
+    }
+}
