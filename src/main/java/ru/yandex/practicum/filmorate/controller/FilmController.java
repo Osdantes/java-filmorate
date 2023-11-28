4c23fd64fa25955ec0da36efe20a19fbc0c65c19
Osdantes <121436438+Osdantes@users.noreply.github.com>
2023-11-28 18:53:02 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.annotations.IsBeforeBirthdayMovie;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
