76d00cfdccbc23421ee43b68bdd0fab919f576ed
Osdantes <121436438+Osdantes@users.noreply.github.com>
2024-01-15 20:36:25 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.MpaService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/mpa")
+@RequiredArgsConstructor
+public class MpaController {
+    private final MpaService mpaService;
+
+    @PostMapping
+    public Mpa addMpa(@RequestBody Mpa mpa) {
+        return mpaService.addMpa(mpa);
+    }
+
+    @PutMapping
+    public Mpa updateMpa(@RequestBody Mpa mpa) {
+        return mpaService.updateMpa(mpa);
+    }
+
+    @GetMapping
+    public List<Mpa> getAllMpa() {
+        return mpaService.getAllMpa();
+    }
+
+    @GetMapping("/{id}")
+    public Mpa getMpaById(@PathVariable int id) {
+        return mpaService.getMpaById(id);
+    }
+}
