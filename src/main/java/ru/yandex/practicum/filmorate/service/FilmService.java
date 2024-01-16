db1d751d23a221c1f09f40f5bb0da5e5b696dc76
marina-svdv <137892951+marina-svdv@users.noreply.github.com>
2024-01-16 13:45:06 +0300

    Update FilmService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -80,7 +80,8 @@ public class FilmService {
     public List<Film> searchFilms(String query, String by) {
         log.info(String.format("Список фильмов с подстрокой query = %s по популярности.", query));
         return filmStorage.searchFilms(query, by);
-
+    }
+    
     public boolean existsById(long filmId) {
         return !isIncorrectId(filmId) && filmStorage.existsById(filmId);
     }
