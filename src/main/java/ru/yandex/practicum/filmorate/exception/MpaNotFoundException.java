f234d0367b8369f65a5ac16d2a2cab4e289f2d9a
Anton Osadchenko <121436438+Osdantes@users.noreply.github.com>
2024-01-17 20:15:51 +0300

    Delete src/main directory

--- src/main/java/ru/yandex/practicum/filmorate/exception/MpaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class MpaNotFoundException extends RuntimeException {
-    public MpaNotFoundException(String msg) {
-        super(msg);
-    }
-}
