ac1f050820d09f18b8e3521131d36a1017cf405c
Anton Osadchenko <121436438+Osdantes@users.noreply.github.com>
2024-01-20 03:57:11 +0300

    Update FeedDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/FeedDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/FeedDbStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage.dao;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
